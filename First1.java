d3e540df1b0f5474a10be93c8ea73b065d0fe178
KaustubhBande <100848390+KaustubhBande@users.noreply.github.com>
2022-03-03 18:23:35 +0530

    Add files via upload

---
+++ First1.java
@@ -0,0 +1,11 @@
+class First1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to the Coding World");
+		System.out.print("Welcome to the Coding World..");
+		System.out.print("Welcome to the Coding World..");
+		System.out.println("Welcome to the Coding World");
+		System.out.println("Welcome to the Coding World");
+	}
+}
